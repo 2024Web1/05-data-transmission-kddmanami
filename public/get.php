4c9b3b1df9851744c38a30f0dd18d01ac7563aed
{kddmanami} <{kd1336527@st.kobedenshi.ac.jp}>
2024-05-07 10:27:35 +0900

    追加

---
+++ public/get.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>GETメソッドのデータ</title>
+    </head>
+    <body>
+        <h4>0J02006 川野愛実</h4>
+        <p>GETメソッドで送られてきたパラメータ名dataの個は、
+            <?php
+            echo $_GET['data'] . "です。</p>";
+            ?>
+    </body>
+</html>
